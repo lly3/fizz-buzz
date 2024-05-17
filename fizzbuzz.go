3e0a15b7597b5ecf51d538cf4bc4eb7d3cd7f1d4
Vittaya Pimta <vittaya.p@arise.tech>
2024-05-17 16:46:18 +0700

    [Level 1]: Pass the cases that the nubmer cant devide by 3 or 5 then should return that number as string

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -3,12 +3,5 @@ package fizzbuzz
 import "fmt"
 
 func fizzbuzz(n int) string {
-	if n%15 == 0 {
-		return "FizzBuzz"
-	} else if n%3 == 0 {
-		return "Fizz"
-	} else if n%5 == 0 {
-		return "Buzz"
-	}
 	return fmt.Sprint(n)
 }
--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -3,43 +3,21 @@ package fizzbuzz
 import "testing"
 
 func TestFizzBuzz(t *testing.T) {
-	// Given
 	cases := []struct {
 		name string
 		n    int
 		want string
 	}{
 		{
-			name: "should return '1' when given input equal '1'",
+			name: "should return number as string when given the number which cant devide by 3 or 5",
 			n:    1,
 			want: "1",
 		},
-		{
-			name: "should return '2' when given input equal '2'",
-			n:    2,
-			want: "2",
-		},
-		{
-			name: "should return 'Fizz' when given input equal '3'",
-			n:    3,
-			want: "Fizz",
-		},
-		{
-			name: "should return 'Buzz' when given input equal '5'",
-			n:    5,
-			want: "Buzz",
-		},
-		{
-			name: "should return 'FizzBuzz' when given n equal 15",
-			n:    15,
-			want: "FizzBuzz",
-		},
 	}
-	// Assert
 	for _, c := range cases {
 		got := fizzbuzz(c.n)
 		if got != c.want {
-			t.Errorf("Want '%s', got '%s'", c.want, got)
+			t.Errorf("want: %v, got: %v", c.want, got)
 		}
 	}
 }
