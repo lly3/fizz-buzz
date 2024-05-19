df4e06dbe5b092dee3ce93b3112740e62089f0e9
Vittaya Pimta <vittaya.p@arise.tech>
2024-05-19 20:17:32 +0700

    [Level 2]: Pass the cases that should return 'FizzBuzz' when given the number that can devide by both 3 and 5

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -55,6 +55,11 @@ func TestFizzBuzz(t *testing.T) {
 			n:    5,
 			want: "Buzz",
 		},
+		{
+			name: "should return 'FizzBuzz' when given the number that can devide by 5 and 3",
+			n:    15,
+			want: "FizzBuzz",
+		},
 	}
 
 	for _, c := range cases {
