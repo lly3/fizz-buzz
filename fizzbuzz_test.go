b7d44f3f25956718457b1f7889ce22db68c68abe
Vittaya Pimta <vittaya.p@arise.tech>
2024-05-19 20:06:22 +0700

    [Level 2]: Pass the cases that when given the number that cant devide by 3 or 5 should return that number as string

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -9,26 +9,12 @@ func TestFizzBuzz(t *testing.T) {
 		want string
 	}{
 		{
-			name: "should return number as string when given the number which cant devide by 3 or 5",
+			name: "should return 1 when given the number that cant devide by 3 or 5",
 			n:    1,
 			want: "1",
 		},
-		{
-			name: "should return 'Fizz' when given the number that can divide by 3",
-			n:    3,
-			want: "Fizz",
-		},
-		{
-			name: "should return 'Buzz' when given the number that can divide by 5",
-			n:    5,
-			want: "Buzz",
-		},
-		{
-			name: "should return 'FizzBuzz' when given the number that can divide by 5 and 3",
-			n:    15,
-			want: "FizzBuzz",
-		},
 	}
+
 	for _, c := range cases {
 		got := fizzbuzz(c.n)
 		if got != c.want {
