4c9a7f0e643ea2e5e0bb9c785989967d8c134f42
Vittaya Pimta <vittaya.p@arise.tech>
2024-05-23 17:14:42 +0700

    [Level 5]: pass the cases that should return 'Fizz' when given number is dividable by 3

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -2,17 +2,37 @@ package fizzbuzz
 
 import "fmt"
 
-type FizzBuzz struct{}
+type Fizz struct{}
 
-func NewFizzBuzz() FizzBuzz {
-	return FizzBuzz{}
+func NewFizz() Fizz {
+	return Fizz{}
+}
+
+func (f Fizz) Run(n int) string {
+	if n%3 == 0 {
+		return "Fizz"
+	}
+	return ""
+}
+
+type FizzBuzz struct {
+	f Fizz
+}
+
+func NewFizzBuzz(f Fizz) FizzBuzz {
+	return FizzBuzz{f}
 }
 
 func (fb FizzBuzz) Run(n int) string {
-	return fmt.Sprint(n)
+	s := fb.f.Run(n)
+	if s == "" {
+		return fmt.Sprint(n)
+	}
+	return s
 }
 
 func fizzbuzz(n int) string {
-	fb := NewFizzBuzz()
+	f := NewFizz()
+	fb := NewFizzBuzz(f)
 	return fb.Run(n)
 }
