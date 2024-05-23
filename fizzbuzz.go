bc42caa89901559eaa005fef7099e29efd592dbb
Vittaya Pimta <vittaya.p@arise.tech>
2024-05-23 17:09:56 +0700

    [Level 5]: pass the cases that should return number as string when given the number that cannot divide by 3 and 5

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -2,15 +2,17 @@ package fizzbuzz
 
 import "fmt"
 
-func ternary(condition bool, a, b interface{}) interface{} {
-	m := map[bool]interface{}{
-		true:  a,
-		false: b,
-	}
-	return m[condition]
+type FizzBuzz struct{}
+
+func NewFizzBuzz() FizzBuzz {
+	return FizzBuzz{}
+}
+
+func (fb FizzBuzz) Run(n int) string {
+	return fmt.Sprint(n)
 }
 
 func fizzbuzz(n int) string {
-	s := fmt.Sprint(ternary(n%3 == 0, "Fizz", "")) + fmt.Sprint(ternary(n%5 == 0, "Buzz", ""))
-	return fmt.Sprint(ternary(s == "", fmt.Sprint(n), s))
+	fb := NewFizzBuzz()
+	return fb.Run(n)
 }
