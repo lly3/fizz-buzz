9568b31433ab0d83984e3fe99fdb600b85f9ac60
Vittaya Pimta <vittaya.p@arise.tech>
2024-05-23 15:27:54 +0700

    [Level 3]: pass the cases that when given the number that can divide by 3 should return 'Fizz'

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -2,6 +2,15 @@ package fizzbuzz
 
 import "fmt"
 
+func ternary(condition bool, a, b interface{}) interface{} {
+	m := map[bool]interface{}{
+		true:  a,
+		false: b,
+	}
+	return m[condition]
+}
+
 func fizzbuzz(n int) string {
-	return fmt.Sprint(n)
+	s := ternary(n%3 == 0, "Fizz", "")
+	return fmt.Sprint(ternary(s == "", fmt.Sprint(n), s))
 }
