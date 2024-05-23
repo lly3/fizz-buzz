9579dc3b31925c0c88b556a3335c2c7cadee9384
Vittaya Pimta <vittaya.p@arise.tech>
2024-05-23 15:30:50 +0700

    [Level 3]: pass the cases that when given the number that can divide by 5 should return 'Buzz'

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -11,6 +11,6 @@ func ternary(condition bool, a, b interface{}) interface{} {
 }
 
 func fizzbuzz(n int) string {
-	s := ternary(n%3 == 0, "Fizz", "")
+	s := fmt.Sprint(ternary(n%3 == 0, "Fizz", "")) + fmt.Sprint(ternary(n%5 == 0, "Buzz", ""))
 	return fmt.Sprint(ternary(s == "", fmt.Sprint(n), s))
 }
